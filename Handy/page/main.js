b9e8b606553f392b823f1d313abef326cd47c4cf
kimsumi701 <64828310+kimsumi701@users.noreply.github.com>
2023-04-05 23:11:50 +0900

    테스트

--- Handy/page/main.js
+++
@@ -1,6 +0,0 @@
-let menu = document.getElementsByClassName('menu');
-let menuCloseBtn = document.getElementById('close-button');
-
-menuCloseBtn.addEventListener('click', function () {
-    menu[0].style.right = '-100%';
-});
